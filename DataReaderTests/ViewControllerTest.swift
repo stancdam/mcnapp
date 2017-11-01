78208c9325016b842c943450a3e3ca267b9578c7
Damian Stanczyk <damian.stanczyk@gmail.com>
2017-11-01 11:37:22 +0100

    First test added

--- DataReaderTests/ViewControllerTest.swift
+++ DataReaderTests/ViewControllerTest.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import XCTest
+@testable import DataReader
+
+class ViewControllerTest: XCTestCase {
+    
+    func test_viewDidLoad_withNoData() {
+        let sut = ViewController()
+        sut.textObjects = Array.createDataModel(size: 0)
+        _ = sut.view
+        
+        XCTAssertEqual(sut.textObjects.count, 0)
+    }
+}
