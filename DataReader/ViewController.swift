eef16df2c0fac76e4040d5e0bd271f03f7e4ddf4
Damian Stanczyk <damian.stanczyk@gmail.com>
2017-11-02 23:08:45 +0100

    Async stuff wrrrrrrr, crash

--- DataReader/ViewController.swift
+++ DataReader/ViewController.swift
@@ -12,8 +12,9 @@ import UIKit
 
 class ViewController: UITableViewController {
     
-    var textObjects = Array.createDataModel(size: numOfCells)
+    var textObjects  = Array.createDataModel(size: numOfCells)
     let cellId = "Cell"
+    let jsonUrlString = "https://private-5e934f-datatextapi.apiary-mock.com/data"
     var timer: Timer?
     
     override func viewDidLoad() {
@@ -21,6 +22,58 @@ class ViewController: UITableViewController {
         tableView.tableFooterView = UIView()
         
         //enableDataChangeFeature()
+        
+        fetchData()
+        
+        print(textObjects)
+        
+    }
+    
+    func fetchData() {
+        guard let url = URL(string: jsonUrlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            // perhaps check ere
+            // check response status
+            
+            guard let data = data else { return }
+            
+            do {
+                
+                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String]
+                if let stringArray = json {
+                    //self.textObjects = stringArray
+                    self.updateAllCells(dataModel: stringArray)
+                }
+                
+                //print(self.textObjects)
+                
+            } catch let jsonErr {
+                print("JSON serialiazing errror", jsonErr)
+            }
+            
+            
+            }.resume()
+    }
+    
+    func updateCell(atRow: Int) {
+        let row = Int(arc4random_uniform(UInt32(atRow)))
+        
+        guard let cell = tableView.cellForRow(at: IndexPath(item: row, section: 0)) as? DataTextViewCell else {
+            return
+        }
+        
+        textObjects[row] = String.randomSentence()
+        
+        tableView.beginUpdates()
+        cell.dataTextView.text = textObjects[row]
+        tableView.endUpdates()
+    }
+    
+    func updateAllCells(dataModel: [String]) {
+        textObjects = dataModel
+        for index in 0..<textObjects.count {
+            updateCell(atRow: index)
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
